3f1c788ab5be10910d2cfaef05ac22d3acc97aa3
Vineeth Raja Banala <84836595+sidhu7777@users.noreply.github.com>
2025-03-18 23:36:35 +0000

    Update database_initialize.sql

--- Data Warehouse Development/database_initialize.sql
+++ Data Warehouse Development/database_initialize.sql
@@ -1,1 +1,42 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'SalesDataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'SalesDataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
 
+USE master;
+GO
+
+-- Drop and recreate the 'SalesDataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'SalesDataWarehouse')
+BEGIN
+    ALTER DATABASE SalesDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE SalesDataWarehousee;
+END;
+GO
+
+-- Create the 'SalesDataWarehousee' database
+CREATE DATABASE SalesDataWarehouse;
+GO
+
+USE SalesDataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
